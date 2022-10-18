0737cd5d2f640774971afd55949aa7b13a46b097
Cristina <georgianacristinat@gmail.com>
2022-10-18 12:52:50 +0200

    functiona?

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,20 @@
 <template>
-  <h1>App</h1>
+  <h1>Task App</h1>
+<ul>
+  <li>
+    <router-link :to="{name: 'home'}">Home</router-link>
+  </li>
+  <li>
+    <router-link :to="{name: 'login'}">Login</router-link>
+  </li>
+</ul>
+<router-view></router-view>
 </template>
 <script setup>
-console.log(process.env.NODE_ENV)
+import{createClient} from '@supabase/supabase-js'
+const supabase = createClient(import.meta.env.VITE_SUPABASE_URL, import.meta.env.VITE_SUPABASE_KEY)
+console.log(supabase)
+
 </script>
 <style scoped>
 </style>
